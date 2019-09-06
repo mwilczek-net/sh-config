9eea71023e82d5e9452e5be16074c74bbbe0d839
mwilczek-net <mwilczek-net@users.noreply.github.com>
2019-09-06 13:50:47 +0200

    Scripts paths

--- OS X/bash_templates/absolute_script_path.sh
+++ OS X/bash_templates/absolute_script_path.sh
@@ -2,4 +2,7 @@
 
 CURRENT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
 
-echo $CURRENT_PATH
+PATH_OF_SCRIPT="$(cd "$(dirname "$BASH_SOURCE")" && pwd -P)"
+
+echo "Current script: ${CURRENT_PATH}"
+echo "Current file even if script sourced with \". SCRIPT_PATH\": ${PATH_OF_SCRIPT}"
