077337cd376b7aa8ea54a7707b4a17e5d64ea162
mwilczek-net <mwilczek-net@users.noreply.github.com>
2023-07-01 00:58:47 +0200

    Attempt  to unify prompt experience

--- allOS/source_bash/100000_prompt_with_git_branch.sh
+++ allOS/source_bash/100000_prompt_with_git_branch.sh
@@ -10,22 +10,23 @@ function git_names {
   local LIGHT_GREEN="\[\033[1;32m\]"
   local       WHITE="\[\033[0m\]"
   local  LIGHT_GRAY="\[\033[0;37m\]"
-  case $TERM in
-    xterm*)
-    TITLEBAR='\[\033]0;\u@\h:\w\007\]'
-    ;;
-    *)
-    TITLEBAR=""
-    ;;
-  esac
-
-# PS1="${TITLEBAR}\ # optional - change tab names
-PS1="$GREEN[$WHITE\$(date +%H:%M)$GREEN]\
-$GREEN[$WHITE\u@\h:\W$GREEN]$RED\$(parse_git_branch)\
-$WHITE\$ "
-PS2='> '
-PS4='+ '
+  # case $TERM in
+  #   xterm*)
+  #   local TITLEBAR='\[\033]0;\u@\h:\w\007\]'
+  #   ;;
+  #   *)
+  #   local TITLEBAR=""
+  #   ;;
+  # esac
+  
+  local prompt_time="$GREEN[$WHITE\t$GREEN]"
+  local prompt_path="$GREEN[$WHITE\w$GREEN]"
+  
+  # PS1="${TITLEBAR}\ # optional - change tab names
+  PS1="${prompt_time}${prompt_path}${RED}\$(parse_git_branch)${WHITE}\$ "
+  PS2='> '
+  PS4='+ '
 }
 
-
 git_names;
+
