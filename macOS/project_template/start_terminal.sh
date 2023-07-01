eb8ec65677cdf96d6fe7893a200bbfaa38778a3d
mwilczek-net <mwilczek-net@users.noreply.github.com>
2023-07-02 01:12:36 +0200

    Attempt to unify project_template experience

--- macOS/project_template/start_terminal.sh
+++ macOS/project_template/start_terminal.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Requires new_tab command. which can be found in bin directory
-
 CURRENT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
+
+# Overwrite default shell for gnome terminal to open scripts with desired shell
+# SHELL='/bin/zsh'
 
 new_tab "Tab 1" ". '${CURRENT_PATH}/start_terminal/script_01.sh'"
 
--- macOS/project_template/start_terminal/script_01.sh
+++ macOS/project_template/start_terminal/script_01.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-CURRENT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
+# In case of importing script with '. SCRIPT_NAME'
+case $(basename "${SHELL}") in
+  bash)
+    echo 'Current shell BASH'
+    CURRENT_PATH="$(cd "$(dirname "$BASH_SOURCE")" && pwd -P)"
+    ;;
+  zsh)
+    echo 'Current shell ZSH'
+    CURRENT_PATH="$(cd "$(dirname "${(%):-%N}")" && pwd -P)"
+    ;;
+  *)
+    echo 'Unknown shell'
+    CURRENT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
+    ;;
+esac
+
 PROJECT_DESTINATION="${CURRENT_PATH}/../"
+
 
 echo "Welcome in script 01"
 
