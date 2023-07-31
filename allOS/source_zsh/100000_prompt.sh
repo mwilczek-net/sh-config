087ca1d3e51e595cd90c9fde89cf844d1eeb0026
mwilczek-net <mwilczek-net@users.noreply.github.com>
2023-07-31 13:12:29 +0200

    Add date to zsh prompt

--- allOS/source_zsh/100000_prompt.sh
+++ allOS/source_zsh/100000_prompt.sh
@@ -4,7 +4,7 @@ precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
 RPROMPT="\$vcs_info_msg_0_ %(?.%F{green}√.%F{red}?%?)%f"
-PROMPT='%F{green}[%f%*%F{green}]%f%F{green}[%f%~%f%b%F{green}]%f%# '
+PROMPT='%F{green}[%f%D %*%F{green}]%f%F{green}[%f%~%f%b%F{green}]%f%# '
 zstyle ':vcs_info:git:*' formats '%F{red}(%b)%f'
 zstyle ':vcs_info:*' enable git
 
