af216837d6b0f41d966d4234dd4cbca17c326698
mwilczek-net <mwilczek-net@users.noreply.github.com>
2023-08-22 15:00:45 +0200

    Homebrew script for macOS

---
+++ macOS/homebrew.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install fortune
+brew install cowsay
+brew install toilet
+brew install dialog
