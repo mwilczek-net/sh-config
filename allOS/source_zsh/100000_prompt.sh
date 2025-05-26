8fd08fcffc662d9e8fe7e4be8d9cb1fa0d79c4e9
mwilczek-net <mwilczek-net@users.noreply.github.com>
2025-05-26 11:33:18 +0200

    Disable prompt refresh for zsh

--- allOS/source_zsh/100000_prompt.sh
+++ allOS/source_zsh/100000_prompt.sh
@@ -2,7 +2,10 @@
 autoload -Uz vcs_info
 precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
-setopt prompt_subst
+
+# refresh prompt on each rerender (i.e. tabs switch)
+# setopt prompt_subst
+
 RPROMPT="\$vcs_info_msg_0_ %(?.%F{green}√.%F{red}?%?)%f"
 PROMPT='%F{green}[%f%D %*%F{green}]%f%F{green}[%f%~%f%b%F{green}]%f%# '
 zstyle ':vcs_info:git:*' formats '%F{red}(%b)%f'
