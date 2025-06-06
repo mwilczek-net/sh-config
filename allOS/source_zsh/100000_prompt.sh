ff7a32ddd1035d0b20b5f43c21f0fbc620589a33
mwilczek-net <mwilczek-net@users.noreply.github.com>
2025-06-06 14:07:43 +0200

    ZSH did not update git message

--- allOS/source_zsh/100000_prompt.sh
+++ allOS/source_zsh/100000_prompt.sh
@@ -4,7 +4,7 @@ precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 
 # refresh prompt on each rerender (i.e. tabs switch)
-# setopt prompt_subst
+setopt prompt_subst
 
 RPROMPT="\$vcs_info_msg_0_ %(?.%F{green}√.%F{red}?%?)%f"
 PROMPT='%F{green}[%f%D %*%F{green}]%f%F{green}[%f%~%f%b%F{green}]%f%# '
